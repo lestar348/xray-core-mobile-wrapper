9ab31054cbe985e6bec39c7a1aa0386a71d56179
lestar348 <lestar348@gmail.com>
2023-11-04 11:46:09 +0300

    added its own import of dependencies and add environment settings

--- xray-mobile.go
+++ xray-mobile.go
@@ -2,9 +2,10 @@ package XRay
 
 import (
 	"bytes"
+	"os"
 	"runtime/debug"
 
-	_ "github.com/xtls/xray-core/main/distro/all"
+	_ "github.com/lestar348/xray-core-mobile-wrapper/all_core_packages"
 
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf/serial"
@@ -21,12 +22,36 @@ func SetMemoryLimit() {
 	debug.SetMemoryLimit(30 * 1024 * 1024)
 }
 
+// Ser AssetsDirectory in Xray env
+func SetAssetsDirectory(path string) {
+	os.Setenv("xray.location.asset", path)
+}
+
+// [key] can be:
+// PluginLocation  = "xray.location.plugin"
+// ConfigLocation  = "xray.location.config"
+// ConfdirLocation = "xray.location.confdir"
+// ToolLocation    = "xray.location.tool"
+// AssetLocation   = "xray.location.asset"
+// UseReadV         = "xray.buf.readv"
+// UseFreedomSplice = "xray.buf.splice"
+// UseVmessPadding  = "xray.vmess.padding"
+// UseCone          = "xray.cone.disabled"
+// BufferSize           = "xray.ray.buffer.size"
+// BrowserDialerAddress = "xray.browser.dialer"
+// XUDPLog              = "xray.xudp.show"
+// XUDPBaseKey          = "xray.xudp.basekey"
+func SetXrayEnv(key string, path string) {
+	os.Setenv(key, path)
+}
+
 func StartXray(config []byte, logger Logger) error {
 	conf, err := serial.DecodeJSONConfig(bytes.NewReader(config))
 	if err != nil {
 		logger.LogInput("Config load error: " + err.Error())
 		return err
 	}
+
 	pbConfig, err := conf.Build()
 	if err != nil {
 		return err
@@ -36,6 +61,7 @@ func StartXray(config []byte, logger Logger) error {
 		logger.LogInput("Create XRay error: " + err.Error())
 		return err
 	}
+
 	err = instance.Start()
 	if err != nil {
 		logger.LogInput("Start XRay error: " + err.Error())
